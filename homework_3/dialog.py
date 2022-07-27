0b14af6e58833e2ce6f3711341fc4aa2119832c2
Anna Shkafenko <anna.shkafenko19@gmail.com>
2022-07-27 19:11:48 +0300

    Створення програми, яка веде діалог

---
+++ homework_3/dialog.py
@@ -0,0 +1,26 @@
+print("Привіт, мене звати Джарвіс!\nЯ допоможу тобі виконати елементарні математичні задачі.")
+name = input("Як до тебе звертатись?")
+print("Приємно познайомитись,", name, "!")
+
+p = "+"
+m = "-"
+d = "*"
+de = "/"
+opr = input("Яку операцію ти хочеш виконати?\nВведи її знак")
+if (str(opr) == str(p)):
+    a = float(input("Введи перше число"))
+    b = float(input("Введи друге число"))
+    print(a + b)
+elif (str(opr) == str(m)):
+    a = float(input("Введи перше число"))
+    b = float(input("Введи друге число"))
+    print(a - b)
+elif (str(opr) == str(d)):
+    a = float(input("Введи перше число"))
+    b = float(input("Введи друге число"))
+    print(a * b)
+elif (str(opr) == str(de)):
+    a = float(input("Введи перше число"))
+    b = float(input("Введи друге число"))
+    print(a / b)
+else:print("Я поки що не можу виконати таку операцію")
