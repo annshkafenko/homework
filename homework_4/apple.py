fbbbd0b5432e4ac9c9b127b1fadb7914e28f2ac2
Anna <anna.shkafenko19@gmail.com>
2022-07-30 16:17:18 +0300

    doing homework

--- homework_4/apple.py
+++ homework_4/apple.py
@@ -1,7 +1,7 @@
-#First task
+# First task
 pupils = float(input("Введіть кіл-ть школярів"))
 apple = float(input("Введіть кіл-ть яблук"))
-receive = apple// pupils
+receive = apple // pupils
 print("Кожному з школярів дістанеться ", receive, " яблук")
 remaind = apple % pupils
 print("В корзині залишиться ", remaind, "яблук")
