8032569a8969956d8dde2037338c55317bf64f56
Anna <anna.shkafenko19@gmail.com>
2022-07-30 20:28:31 +0300

    error correction

--- homework_4/pupils.py
+++ homework_4/pupils.py
@@ -1,6 +1,11 @@
 klass1 = float(input("Введіть кількість учнів у 1-му класі"))
 klass2 = float(input("Введіть кількість учнів у 2-му класі"))
 klass3 = float(input("Введіть кількість учнів у 3-му класі"))
-tables = (klass1 + klass2 + klass3) // 2
-remain = (klass1 + klass2 + klass3) % 2
-print("Усього потрібно закупити ", tables + remain, " парт")
+tables_1 = klass1 // 2
+remain_1 = klass1 % 2
+tables_2 = klass2 // 2
+remain_2 = klass2 % 2
+tables_3 = klass3 // 2
+remain_3 = klass3 % 2
+result = tables_1 + remain_1 + tables_2 + remain_2 + tables_3 + remain_3
+print("Усього потрібно закупити ", result, " парт")
