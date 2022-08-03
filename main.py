b65bad70218a7ea13da0e98b27e0293e6e17e0c2
Anna <anna.shkafenko19@gmail.com>
2022-08-03 18:24:45 +0300

    work on lesson

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello  world")
+cnt = 0
+while cnt < 10:
+    cnt += 1
+    print(cnt, "ddd")
+    if cnt % 2:
+        continue
+
+    print("I`M HERE", cnt)
+else:
+    print("Hi")
--- main2.py
+++ main2.py
@@ -1,1 +1,48 @@
-print("Hello world")
+# n = 0
+# while n <= 0:
+#   n = int(input("Введіть позитивне число: "))
+
+# print(n)
+
+# піднесення до степені
+# k = 1
+# while k < 10:
+#   print( 2**k )
+#  k += 1
+
+# range(s, [stop], [step])
+
+# r = range(10, 300, 5)
+# print(r.__sizeof__())
+# n = range(10, 3000000)
+# print(r.__sizeof__())
+
+# print(list(r))
+# v = range(2, 2000)
+# print(v[5])
+
+# for i in range(1, 11):
+#    print(2**i)
+
+# print('/U0001F61A')
+
+import time
+
+for i in range(10):
+    if i % 2:
+        print('\U0001FAF5', end='\r')
+        time.sleep(1)
+    if i % 3:
+        print('\U0001F440', end='\r')
+        time.sleep(1)
+    else:
+        print('\U0000270B', end='\r')
+        time.sleep(1)
+
+# табуляція
+' '
+" "
+""" okfokofkoko
+ jjojfojoj
+ fjojsojf
+ kfks-kk-k-kpsk """
