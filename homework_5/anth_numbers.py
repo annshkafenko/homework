e2d67d056eec7a159ae1aadd04b75fdc77d90691
Anna <anna.shkafenko19@gmail.com>
2022-08-10 22:58:56 +0300

    doing homework

--- homework_5/anth_numbers.py
+++ homework_5/anth_numbers.py
@@ -1,3 +1,7 @@
-n = int(input("Введіть число: "))
-for n in input("Введіть число: "):
-    while
+n = int(input('задайте N: '))
+for i in range(1, n):
+    d = 10
+    while (i >= d):
+      d = d * 10
+    if ((i*i) % d) == i:
+      print('число ', i, 'квадрат ', i*i)
